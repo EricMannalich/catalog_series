b1cd4c6cceb21dacdee4824007d3b5090f9f201b
EricMannalich <ericmannalichcossio@gmail.com>
2024-07-07 14:45:59 -0400

    coment ujson

--- apps/principal/api/api.py
+++ apps/principal/api/api.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse
-import ujson
+# import ujson
 
 from apps.base.api import *
 from apps.base.serializers import *
